8f8486c6a5e7d852cd71e84abc0f49bf7dd6e921
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 16:55:54 -0300

    added new info to class

--- src/app/books-list/entities/book.entity.ts
+++ src/app/books-list/entities/book.entity.ts
@@ -1,14 +1,14 @@
 export class Book {
-  id = '';
-  title = '';
-  description = '';
-  authors: String[] = [];
-  pageCount = '';
-  category = '';
-  imageUrl = '';
-  isbn10 = '';
-  isbn13 = ''
-  language = '';
-  publisher = '';
-  published = 0;
+  id = 'aaaaaaaaaaaaaaaaa';
+  title = 'Crossing the Chasm';
+  description = 'teste aaaaaa';
+  authors: string[] = ['Geoffrey A.Moore'];
+  pageCount = '150';
+  category = 'teste aaaaaa';
+  imageUrl = '../../../assets/book-cover.png';
+  isbn10 = 'teste aaaaaa';
+  isbn13 = 'teste aaaaaa';
+  language = 'teste aaaaaa';
+  publisher = 'Editora Loyola';
+  published = 2020;
 }
