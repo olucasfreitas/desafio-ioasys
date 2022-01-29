963e040124ad0916fb8adc6ee0c9df23572920b9
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 09:59:39 -0300

    added user and book entitys

---
+++ src/app/books-list/entities/book.entity.ts
@@ -0,0 +1,14 @@
+export class Book {
+  id = '';
+  title = '';
+  description = '';
+  authors: String[] = [];
+  pageCount = '';
+  category = '';
+  imageUrl = '';
+  isbn10 = '';
+  isbn13 = ''
+  language = '';
+  publisher = '';
+  published = 0;
+}
