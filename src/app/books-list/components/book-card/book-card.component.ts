e5a56eb37a1f6021f6180e695cc21851df180da5
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 16:55:42 -0300

    styled the book card

--- src/app/books-list/components/book-card/book-card.component.ts
+++ src/app/books-list/components/book-card/book-card.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../entities/book.entity';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
+  book: Book = new Book();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
