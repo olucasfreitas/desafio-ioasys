ac146b2b66987e1fdcad86c737e64451689a3110
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 12:34:41 -0300

    added routes to login and book list pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksListPageComponent } from './books-list/books-list-page/books-list-page.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent},
+  { path: 'books-list', component: BooksListPageComponent},
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
