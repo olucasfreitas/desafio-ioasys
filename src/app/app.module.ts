59ebe565cd16bce5147d83081b3e747f717236b6
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 10:00:15 -0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './login/components/form/form.component';
+import { BookCardComponent } from './books-list/components/book-card/book-card.component';
+import { BookDetailsModalComponent } from './books-list/components/book-details-modal/book-details-modal.component';
+import { BooksListPageComponent } from './books-list/books-list-page/books-list-page.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    BookCardComponent,
+    BookDetailsModalComponent,
+    BooksListPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
