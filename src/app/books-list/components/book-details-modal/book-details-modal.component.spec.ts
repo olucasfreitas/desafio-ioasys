9cb904b966a5c238e3c9a97ed9caeea68ca8afbb
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 09:59:23 -0300

    added booke details component

---
+++ src/app/books-list/components/book-details-modal/book-details-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookDetailsModalComponent } from './book-details-modal.component';
+
+describe('BookDetailsModalComponent', () => {
+  let component: BookDetailsModalComponent;
+  let fixture: ComponentFixture<BookDetailsModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookDetailsModalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookDetailsModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/books-list/components/book-details-modal/book-details-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-details-modal',
+  templateUrl: './book-details-modal.component.html',
+  styleUrls: ['./book-details-modal.component.scss']
+})
+export class BookDetailsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
