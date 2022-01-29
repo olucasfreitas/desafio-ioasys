8d6a79df6f42544b28c40eaed66238560d6ca96f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-29 09:58:55 -0300

    added books list page

---
+++ src/app/books-list/books-list-page/books-list-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BooksListPageComponent } from './books-list-page.component';
+
+describe('BooksListPageComponent', () => {
+  let component: BooksListPageComponent;
+  let fixture: ComponentFixture<BooksListPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BooksListPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BooksListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/books-list/books-list-page/books-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books-list-page',
+  templateUrl: './books-list-page.component.html',
+  styleUrls: ['./books-list-page.component.scss']
+})
+export class BooksListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
