27fc2b3338f76026fc854cf25c81d8a00cc18f3f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-30 16:00:50 -0300

    added method to signout, corrected signIn mehtod and added autorization, refresh-token and current user subjects

--- src/app/login/services/user.service.ts
+++ src/app/login/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../entities/user.entity';
 
@@ -9,29 +10,85 @@ import { User } from '../entities/user.entity';
 })
 export class UserService {
   url = `${environment.apiBaseUrl}/auth`;
-  authorization =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MWM5YzI5MGNjNDk4YjVjMDg4NDVlMGEiLCJ2bGQiOjE2NDM0OTQ5NTI3MzUsImlhdCI6MTY0MzQ5ODU1MjczNX0.6U5FU__-xJMGPXkH4m1BEVC4hGXIS3jB3qmlLhJ2Qrs';
+  authorizationToken = '';
+  private authorizationTokenSubject = new BehaviorSubject<string>(this.authorizationToken);
+  refreshToken = '';
+  private refreshTokenSubject = new BehaviorSubject<string>(this.refreshToken);
+  private currentUser = new BehaviorSubject<User>(new User());
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
+
+  setCurrentUser(value: User): void {
+    return this.currentUser.next(value);
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.currentUser.asObservable();
+  }
+
+  setAuthorizationToken(value: string): void {
+    this.authorizationToken = value;
+    return this.authorizationTokenSubject.next(value);
+  }
+
+  getAuthorizationToken(): Observable<string> {
+    return this.authorizationTokenSubject.asObservable();
+  }
+
+  setRefreshToken(value: string): void {
+    this.refreshToken = value;
+    return this.refreshTokenSubject.next(value);
+  }
+
+  getRefreshToken(): Observable<string> {
+    return this.refreshTokenSubject.asObservable();
+  }
 
   signIn(email: string, password: string): Observable<User> {
     const user = { email, password };
-    const httpOptions = {
-      headers: new HttpHeaders({
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        Authorization: this.authorization,
-      }),
-    };
-    return this.http.post<User>(`${this.url}/sign-in`, user, httpOptions);
+
+    return this.http
+      .post(`${this.url}/sign-in`, user, {
+        headers: new HttpHeaders({
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          authorization: this.authorizationToken,
+        }),
+        observe: 'response',
+      })
+      .pipe(
+        map((res) => {
+          const authToken = JSON.stringify(res.headers.get('Authorization'));
+          const refreshToken = JSON.stringify(res.headers.get('refresh-token'));
+
+          localStorage.setItem('authToken', authToken);
+          localStorage.setItem('refreshToken', refreshToken);
+          localStorage.setItem('currentUser', JSON.stringify(res.body));
+
+          this.currentUser.next(res.body as User);
+          this.authorizationTokenSubject.next(authToken);
+          this.refreshTokenSubject.next(refreshToken);
+
+          this.router.navigate(['/books-list']);
+
+          return res.body as User;
+        }),
+      );
   }
 
-  generateNewToken(token: string): Observable<string> {
+  signOut() {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('authToken');
+    this.currentUser.next(new User());
+    this.router.navigate(['/login']);
+  }
+
+  authorizeRefreshToken(token: string): Observable<void> {
     const httpOptions = {
       headers: new HttpHeaders({
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        Authorization: this.authorization,
+        Authorization: this.authorizationToken,
       }),
     };
-    return this.http.post<string>(`${this.url}/refresh-token`, token, httpOptions);
+    return this.http.post<void>(`${this.url}/refresh-token`, token, httpOptions);
   }
 }
