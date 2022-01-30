35b61ff6889bc7fec5ed637d1fd0cb9524edf119
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-30 15:59:59 -0300

    added verification to see if user is logged

--- src/app/login/login-page/login-page.component.ts
+++ src/app/login/login-page/login-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-  }
+    const user = localStorage.getItem('currentUser');
 
+    if (user) {
+      this.router.navigate(['/books-list']);
+    }
+  }
 }
