e5d6b932394bbb645a9618ea4e9d563a483c5e97
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-30 15:56:42 -0300

    added verification to see if user if logged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './login/entities/user.entity';
+import { UserService } from './login/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'desafio-ioasys';
+export class AppComponent implements OnInit {
+  user = new User();
+  authToken = '';
+  refreshToken = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    const localUser = localStorage.getItem('currentUser');
+    if (localUser) {
+      this.user = JSON.parse(localUser);
+      this.userService.setCurrentUser(this.user);
+    }
+
+    const localAuthToken = localStorage.getItem('authToken');
+    if (localAuthToken) {
+      this.authToken = JSON.parse(localAuthToken);
+      this.userService.setAuthorizationToken(this.authToken);
+    }
+  }
 }
