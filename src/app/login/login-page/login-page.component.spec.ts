34bb3ac5276bf3cf6333aa2f951230d640873b2f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 00:02:45 -0300

    added unit tests to login-page

--- src/app/login/login-page/login-page.component.spec.ts
+++ src/app/login/login-page/login-page.component.spec.ts
@@ -1,24 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { User } from '../entities/user.entity';
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: Router, useValue: routerSpy }],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
+    const store: any = {};
+    spyOn(localStorage, 'getItem').and.callFake((key: string): string => {
+      return store[key] || null;
+    });
+    spyOn(localStorage, 'setItem').and.callFake((key: string, value: string): string => {
+      return (store[key] = <string>value);
+    });
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    localStorage.setItem('currentUser', JSON.stringify(new User()));
+    router.navigate = jasmine.createSpy().and.resolveTo({});
+    component.ngOnInit();
     expect(component).toBeTruthy();
   });
 });
