7dee13c75131bb08d11cf7f2dc67552dddca903e
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 11:49:13 -0300

    added book details unit tests

--- src/app/books-list/components/book-details-modal/book-details-modal.component.spec.ts
+++ src/app/books-list/components/book-details-modal/book-details-modal.component.spec.ts
@@ -3,19 +3,25 @@ import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/materia
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BookDetailsModalComponent } from './book-details-modal.component';
+import { BooksService } from '../../services/books.service';
+import { Book } from '../../entities/book.entity';
+import { of } from 'rxjs';
 
 describe('BookDetailsModalComponent', () => {
   let component: BookDetailsModalComponent;
   let fixture: ComponentFixture<BookDetailsModalComponent>;
+  let booksService: jasmine.SpyObj<BooksService>;
 
   beforeEach(async () => {
     const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    const booksServiceSpy = jasmine.createSpyObj('BooksService', ['getSpecificBook']);
     await TestBed.configureTestingModule({
       declarations: [BookDetailsModalComponent],
       imports: [MatDialogModule, HttpClientTestingModule],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: { data: MAT_DIALOG_DATA } },
         { provide: MatDialogRef, useValue: dialogRefSpy },
+        { provide: BooksService, useValue: booksServiceSpy },
       ],
     }).compileComponents();
   });
@@ -23,11 +29,20 @@ describe('BookDetailsModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookDetailsModalComponent);
     component = fixture.componentInstance;
+    booksService = TestBed.inject(BooksService) as jasmine.SpyObj<BooksService>;
+    booksService.getSpecificBook.and.returnValue(of(new Book()));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load book on init', () => {
+    booksService.getSpecificBook.and.returnValue(of(new Book()));
+    component.ngOnInit();
+    expect(booksService.getSpecificBook).toHaveBeenCalled();
+    expect(component.book).toEqual(new Book());
   });
 
   it('should close modal', () => {
