265d02cdc947ff6f4f4974d44858f52202e582ad
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 00:01:36 -0300

    added unit tests to book-details

--- src/app/books-list/components/book-details-modal/book-details-modal.component.spec.ts
+++ src/app/books-list/components/book-details-modal/book-details-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BookDetailsModalComponent } from './book-details-modal.component';
 
@@ -7,8 +9,14 @@ describe('BookDetailsModalComponent', () => {
   let fixture: ComponentFixture<BookDetailsModalComponent>;
 
   beforeEach(async () => {
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
     await TestBed.configureTestingModule({
       declarations: [BookDetailsModalComponent],
+      imports: [MatDialogModule, HttpClientTestingModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { data: MAT_DIALOG_DATA } },
+        { provide: MatDialogRef, useValue: dialogRefSpy },
+      ],
     }).compileComponents();
   });
 
@@ -20,5 +28,10 @@ describe('BookDetailsModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close modal', () => {
+    component.close();
+    expect(component.dialogRef.close).toHaveBeenCalled();
   });
 });
