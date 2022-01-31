275d240da50c30460d5cf047daf7132acaa549e5
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 00:02:10 -0300

    added necessary modules

--- src/app/books-list/services/books.service.spec.ts
+++ src/app/books-list/services/books.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BooksService } from './books.service';
 
@@ -6,7 +7,7 @@ describe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(BooksService);
   });
 
--- src/app/login/components/form/form.component.spec.ts
+++ src/app/login/components/form/form.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FormComponent } from './form.component';
 
@@ -9,6 +12,7 @@ describe('FormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FormComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
   });
 
