cabf1724c30e83af70cb6d8bdd190928aeac5430
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 00:00:50 -0300

    added unit tests to book-list-page

--- src/app/books-list/books-list-page/books-list-page.component.spec.ts
+++ src/app/books-list/books-list-page/books-list-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BooksListPageComponent } from './books-list-page.component';
 
@@ -9,6 +11,7 @@ describe('BooksListPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BooksListPageComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
   });
 
@@ -20,5 +23,21 @@ describe('BooksListPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test if screen is bigger then 440px', () => {
+    component.setMatDrawerMode(750);
+    expect(component.isMobile).toEqual(false);
+  });
+  it('should test if screen is smaller then 440px', () => {
+    component.setMatDrawerMode(400);
+    expect(component.isMobile).toEqual(true);
+  });
+  it('should scroll to the top of the page', () => {
+    jasmine.clock().install();
+    component.scrollTop();
+    jasmine.clock().tick(100);
+    expect(component.scroll.nativeElement.scrollTop).toEqual(0);
+    jasmine.clock().uninstall();
   });
 });
