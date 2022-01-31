9e95ed68c41d3f056eb0303194ab654e60efc5c2
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 11:49:21 -0300

    added book service unit tests

--- src/app/books-list/services/books.service.spec.ts
+++ src/app/books-list/services/books.service.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { BooksService } from './books.service';
+import { Book, BooksResponse } from '../entities/book.entity';
 
 describe('BooksService', () => {
-  let service: BooksService;
+  let booksService: BooksService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
-    service = TestBed.inject(BooksService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    booksService = TestBed.inject(BooksService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(booksService).toBeTruthy();
+  });
+
+  it('Should GET all the books from a category', () => {
+    const expected = new BooksResponse();
+    booksService.getBooks(1, 1, 'biographies', 'token').subscribe((data) => expect(data).toEqual(expected));
+
+    const req = httpTestingController.expectOne(`${booksService.url}?page=1&amount=1&category=biographies`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expected);
+  });
+
+  it('Should GET a specific book', () => {
+    const expected = new Book();
+    booksService.getSpecificBook('id', 'token').subscribe((data) => expect(data).toEqual(expected));
+
+    const req = httpTestingController.expectOne(`${booksService.url}/id`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expected);
   });
 });
