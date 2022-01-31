734d6866a017fd566749448e3b8aab963841af4b
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-01-31 11:49:36 -0300

    added form component unit tests

--- src/app/login/components/form/form.component.spec.ts
+++ src/app/login/components/form/form.component.spec.ts
@@ -4,25 +4,46 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { FormComponent } from './form.component';
+import { UserService } from '../../services/user.service';
+import { of } from 'rxjs';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let userService: jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj('UserService', ['signIn', 'setCurrentUser']);
+
     await TestBed.configureTestingModule({
       declarations: [FormComponent],
       imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [{ provide: UserService, useValue: userServiceSpy }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    const store: any = {};
+    spyOn(localStorage, 'getItem').and.callFake((key: string): string => {
+      return store[key] || null;
+    });
+    spyOn(localStorage, 'setItem').and.callFake((key: string, value: string): string => {
+      return (store[key] = <string>value);
+    });
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    userService.signIn.and.returnValue(of());
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should sign user in and define items on storage', () => {
+    userService.signIn.and.returnValue(of());
+    component.onSubmit();
+    expect(userService.signIn).toHaveBeenCalled();
   });
 });
