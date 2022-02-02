e8f275248ce049b58d74c093a0f9c997e4021abd
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-02 10:56:28 -0300

    removed uncessary code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [],
       declarations: [AppComponent],
     }).compileComponents();
-    const store: any = {};
-
-    spyOn(localStorage, 'getItem').and.callFake((key: string): string => {
-      return store[key] || null;
-    });
-    spyOn(localStorage, 'setItem').and.callFake((key: string, value: string): string => {
-      return (store[key] = <string>value);
-    });
-
     fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
@@ -31,29 +17,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should set current user', () => {
-    const user = {
-      id: '',
-      name: '',
-      email: '',
-      birthDate: '',
-      gender: '',
-      authorizationToken: 'dasdasdas',
-      refreshToken: 'dasdasdas',
-    };
-    localStorage.setItem('currentUser', JSON.stringify(user));
-    localStorage.setItem('authorization', user.authorizationToken);
-    localStorage.setItem('refresh-token', user.refreshToken);
-
-    component.ngOnInit();
-
-    expect(localStorage.getItem('currentUser')).toEqual(JSON.stringify(user));
-    expect(localStorage.getItem('authorization')).toEqual('dasdasdas');
-    expect(localStorage.getItem('refresh-token')).toEqual('dasdasdas');
-    expect(component.user).toEqual(user);
-    expect(component.user.authorizationToken).toEqual('dasdasdas');
-    expect(component.user.refreshToken).toEqual('dasdasdas');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from './login/entities/user.entity';
-import { UserService } from './login/services/user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  user = new User();
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    const localUser = localStorage.getItem('currentUser');
-    const authToken = localStorage.getItem('authorization');
-    const refreshToken = localStorage.getItem('refresh-token');
-
-    if (localUser) {
-      this.user = JSON.parse(localUser);
-      this.user.authorizationToken = authToken as string;
-      this.user.refreshToken = refreshToken as string;
-      this.userService.setCurrentUser(this.user);
-    }
-  }
-}
+export class AppComponent {}
